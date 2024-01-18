540b8359c21f5e259fac718ab34f4f2e4aae86c6
Zain Bin Rizwan <zainbinrizwan4@gmail.com>
2024-01-18 07:07:00 +0500

    chatbot

---
+++ gemini_app_chatbot.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import google.generativeai as genai
+
+st.title("ChatGPT-like clone with Gemini")
+
+# Configure Gemini API
+genai.configure(api_key="AIzaSyAaW1FgmWvCwkouc18yZ2GeAXNyWXcc6VQ")  # Replace with your API key
+
+# Set up model configuration
+generation_config = {
+    "temperature": 0.9,
+    "top_p": 1,
+    "top_k": 1,
+    "max_output_tokens": 2048,
+}
+safety_settings = [
+    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"}
+]
+model = genai.GenerativeModel(
+    model_name="gemini-pro", generation_config=generation_config, safety_settings=safety_settings
+)
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        prompt_parts = [{"text": prompt}]
+        response = model.generate_content(prompt_parts)
+        for candidate in response.candidates:
+            for part in candidate.content.parts:
+                full_response += part.text
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
